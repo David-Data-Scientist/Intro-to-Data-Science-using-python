d1f1a4a2e32ba4567bc316b663737b503554567b
David Huang <dhuhuang@ucdavis.edu>
2016-08-31 19:42:22 +0800

    Update and rename Intro_to_Data_Science_Lesson_3_Project.py to Lesson_3_Project.py

--- Lesson_3_Project.py
+++ Lesson_3_Project.py
@@ -4,6 +4,7 @@ import scipy
 import scipy.stats
 import matplotlib.pyplot as plt
 from ggplot import *
+import statsmodels.api as sm
 
 '''###################################1 - Exploratory Data Analysis#################################'''
 
@@ -224,13 +225,13 @@ def plot_cost_history(alpha, cost_history):
         'Iteration': range(len(cost_history))
     })
     return ggplot(cost_df, aes('Iteration', 'Cost_History')) + geom_point() + \
-           ggtitle('Cost History for alpha = %.3f' % alpha)
+        ggtitle('Cost History for alpha = %.3f' % alpha)
 
 
-'''################################################6 - Plotting Residuals############################################'''
+'''#########################################6 - Plotting Residuals##################################'''
 
 
-def plot_residuals(turnstile_weather, predictions):
+def plot_residuals(turnstile_weather, prediction):
     """
     Using the same methods that we used to plot a histogram of entries
     per hour for our data, why don't you make a histogram of the residuals
@@ -243,3 +244,89 @@ def plot_residuals(turnstile_weather, predictions):
     http://www.itl.nist.gov/div898/handbook/pri/section2/pri24.htm
     """
 
+    plt.figure()
+    # Your code goes here:
+    (turnstile_weather['ENTRIESn_hourly'] - prediction).hist()
+    return plt
+
+
+'''######################################7 - Compute R^2##############################################'''
+
+
+def compute_r_squared(data, prediction):
+    """
+    In exercise 5, we calculated the R^2 value for you. But why don't you try and
+    and calculate the R^2 value yourself.
+
+    Given a list of original data points, and also a list of predicted data points,
+    write a function that will compute and return the coefficient of determination (R^2)
+    for this data.  numpy.mean() and numpy.sum() might both be useful here, but
+    not necessary.
+
+    Documentation about numpy.mean() and numpy.sum() below:
+    http://docs.scipy.org/doc/numpy/reference/generated/numpy.mean.html
+    http://docs.scipy.org/doc/numpy/reference/generated/numpy.sum.html
+    """
+
+    # your code here
+    r_squared = 1 - sum((data - prediction) ** 2) / \
+        sum((data - np.mean(data)) ** 2)
+    return r_squared
+
+
+'''##################################8 - More Linear Regression (Optional)############################'''
+
+"""
+In this optional exercise, you should complete the function called
+predictions(turnstile_weather). This function takes in our pandas
+turnstile weather dataframe, and returns a set of predicted ridership values,
+based on the other information in the dataframe.
+
+In exercise 3.5 we used Gradient Descent in order to compute the coefficients
+theta used for the ridership prediction. Here you should attempt to implement
+another way of computing the coefficients theta. You may also try using a reference implementation such as:
+http://statsmodels.sourceforge.net/devel/generated/statsmodels.regression.linear_model.OLS.html
+
+One of the advantages of the statsmodels implementation is that it gives you
+easy access to the values of the coefficients theta. This can help you infer relationships
+between variables in the dataset.
+
+You may also experiment with polynomial terms as part of the input variables.
+
+The following links might be useful:
+http://en.wikipedia.org/wiki/Ordinary_least_squares
+http://en.wikipedia.org/w/index.php?title=Linear_least_squares_(mathematics)
+http://en.wikipedia.org/wiki/Polynomial_regression
+
+This is your playground. Go wild!
+
+How does your choice of linear regression compare to linear regression
+with gradient descent computed in Exercise 3.5?
+
+You can look at the information contained in the turnstile_weather dataframe below:
+https://s3.amazonaws.com/content.udacity-data.com/courses/ud359/turnstile_data_master_with_weather.csv
+
+Note: due to the memory and CPU limitation of our amazon EC2 instance, we will
+give you a random subset (~10%) of the data contained in turnstile_data_master_with_weather.csv
+
+If you receive a "server has encountered an error" message, that means you are hitting
+the 30 second limit that's placed on running your program. See if you can optimize your code so it
+runs faster.
+"""
+
+
+def predict(weather_turnstile):
+    # Your implementation goes here. Feel free to write additional
+    # helper functions
+    X = weather_turnstile[['rain', 'precipi', 'Hour', 'meantempi']]
+    dummy_units = pandas.get_dummies(weather_turnstile['UNIT'], prefix='unit')
+    # add UNIT to features using dummy variables
+    X = X.join(dummy_units)
+    # add constant '1'
+    X = sm.add_constant(X)
+    Y = weather_turnstile['ENTRIESn_hourly']
+    model = sm.OLS(Y, X)
+    # fit the model
+    results = model.fit()
+    prediction = results.predict()
+    return prediction  # R^2 = 0.4834
